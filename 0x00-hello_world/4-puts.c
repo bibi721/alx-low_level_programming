c4b7d3814add93f5922f20aad06c71da49e85bad
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-08 14:42:59 -0700

    first c program on alx journey!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - function starter
  *
- * Return: Always 0 (Success)
+ * Return: value with 0
+ *
+ *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	return (0);
 }
