3608aaf92afc364948fd6d802f9d6597f449020a
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-09 16:35:14 -0700

    for loop program with c language

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,14 @@
 
 /**
  *
- * main - this is where main starts
+ * main - function goes here
  *
  * Return: Always 0(success)
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		 putchar(ch);
