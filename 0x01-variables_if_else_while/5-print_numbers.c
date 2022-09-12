aaf068ad4700be299ecff98bfe088351f960e218
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-12 16:54:41 -0700

    list base 10 numbers between 0 && 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -print base 10 numbers starting from 0
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	int num = 0;
+
+	while(num < 10)
+	{
+		printf(num);
+		num++;
+	}
+	printf('\n');
+	return (0);
+}
+
+
