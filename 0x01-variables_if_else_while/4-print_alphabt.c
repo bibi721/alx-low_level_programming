e79a4985c92e0eb0898581b96c02749d509dca66
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-12 16:11:25 -0700

    for loop with continue statement c program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is if else and for loop using escape function
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char leter;
+
+	for(leter = 'a'; leter <= 'z' leter++)
+	{
+		if (leter = e || leter = q)
+		{
+			continue;
+		}
+		putchar(leter);
+	}
+	putchar('\n');
+	return (0);
+}
