1a0d4364a01234eb58b3b8fe9119a690b64c26bd
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-12 15:55:03 -0700

    another if else statement c program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function starts here
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 97; i <=122; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j <=90; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
