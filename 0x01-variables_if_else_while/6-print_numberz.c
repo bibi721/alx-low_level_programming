48e51faed8a9cb3fbc21dc854afef0589a0186b1
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-12 17:12:43 -0700

    base 10 numnbers using int instead of char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - your function starts here
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	int numz;
+
+	for(numz = 0; numz <= 9; numz++)
+	{
+		putchar((numz % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
