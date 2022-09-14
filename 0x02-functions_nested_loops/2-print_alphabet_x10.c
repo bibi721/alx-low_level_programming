39e72c333ddebc2309a902a9270a359bb2263221
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-14 13:46:16 -0700

    print all alphabet a to z ten times.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,9 @@ void print_alphabet_x10(void)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-			_putchar(ch);
-		        _putchar('\n');
+			_putchar(cnt);
 		}
+		_putchar('\n');
 		cnt++;
 	}
-
 }
