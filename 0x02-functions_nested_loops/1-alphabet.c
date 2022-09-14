7c35b8fa999319c5e89aae894fba990de6d8c76f
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-14 08:02:32 -0700

    print alpabets from a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - your function goes here
+ * Return: Always 0(success)
+ *
+ */
+void print_alphabet(void)
+{
+	char ltr;
+
+	for ((ltr = 'a'; ltr <= 'z'; ltr++)
+			{
+			_putchar(ltr);
+			}
+			_putchar('\n');
+			return (0);
+			}
