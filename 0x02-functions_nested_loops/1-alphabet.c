a90d8926f3b171b9fcc0d282388f9b1971b66db6
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-14 12:59:05 -0700

    alaphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 /**
  * main - your function goes here
  * Return: Always 0(success)
- *
  */
-void print_alphabet(void)
-{
+void print_alphabet(void
+		{
 	char ltr;
 
 	for (ltr = 'a'; ltr <= 'z'; ltr++)
 			{
-
 			_putchar(ltr);
 			}
+
 			_putchar('\n');
 			}
