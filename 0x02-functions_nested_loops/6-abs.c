f12504b20254c51c6630e036829466c285d11c31
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-14 17:06:08 -0700

    c program that finds an absolute value of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - find absolute value for a number
+ * @:  single letter input
+ * Return: Always 0.
+ *
+ */
+int _abs(int)
+{
+	int number = abs(number);
+
+	return (0);
+}
