e6518944fc39ae8f1c49e4a37a680aa0dac57af9
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-14 17:57:40 -0700

    c program to find last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - find last digit of number
+ * @val: a number that we will find last digit for
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int val)
+{
+	int num;
+
+	val = num % 10;
+
+	return (val);
+}
