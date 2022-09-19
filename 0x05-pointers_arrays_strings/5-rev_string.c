b15bfa2ea0781bcc811ff5afac69a1dde3c50c47
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-19 16:43:49 -0700

    another string reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string pointer
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	char *val = s;
+
+	char v[200];
+
+	int d;
+
+	while (*s != '\0')
+	{
+		v[d] = *s;
+		s++;
+		d++;
+
+	}
+
+	d = 0;
+
+
+	while (s > val)
+	{
+		s--;
+		*s = v[d];
+		d++;
+	}
+}
