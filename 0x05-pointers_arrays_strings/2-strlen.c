273e5f87000cc5838f5f6ef5de8d71b86e396b86
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-19 14:44:40 -0700

    finding a program that fits  function: strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen - find the length of characters in a string
+ * @s: input value c
+ * Return: returns lnth.
+ */
+int _strlen(char *s)
+{
+	int lnth = 0;
+
+	while (*s != '\0')
+	{
+		lnth++;
+		*s++;
+	}
+	return (lnth);
+}
