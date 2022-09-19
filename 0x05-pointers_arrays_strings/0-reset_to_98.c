906363407cbf6c67befc84a66e32001f4cd13845
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-19 12:49:38 -0700

    pointer and changing value it points

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _reset_to_98 - function that takes a pointer to an int as parameter
- * @ptr: pointer input
+ * @*n: pointer input
  * Return: no return
  */
 void reset_to_98(int *n)
