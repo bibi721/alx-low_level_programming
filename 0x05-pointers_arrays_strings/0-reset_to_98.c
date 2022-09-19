3361fefb4e8115ae281b8b6687d9312fd54df25c
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-19 12:44:02 -0700

    changing the value of pointer points

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _reset_to_98 - function that takes a pointer to an int as parameter
+ * @ptr: pointer input
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is another C programming involving pointers, arrays and strings
