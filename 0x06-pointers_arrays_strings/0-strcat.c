ecd93286d29649211882d449c8a869134c1b5890
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-22 08:31:09 -0700

    C program that replaces strcpy ()

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,19 +4,16 @@
  * _strcat - a function that concatenates two strings.
  * @dest: this is the first string input
  * @src: this is the other string input
- * Return: returns pointer
+ * Return: @dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int k, lengz;
-
 	for (lengz = 0; dest[lengz] != '\0'; lengz++)
 		;
 	for (k = 0; src[k] != '\0'; k++)
 		dest[lengz + 1] = src[k];
-
 	dest[lengz + 1] = '\0';
-
+	
 	return (dest);
-
 }
