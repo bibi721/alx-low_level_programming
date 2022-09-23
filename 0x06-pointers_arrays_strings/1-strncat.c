64fb5ed98adfcb9c9e2120963762616c8f0a4008
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-23 12:22:43 -0700

    0x06

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,15 +4,23 @@
  * @dest: destination
  * @src: source string
  * @n: n byytes
- * Return: dest 
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x = 0, opt = 0;
+	int count = 0, count2 = 0;
 
-	while (dest[x++])
-		opt++;
-	for (x = 0; src[x] && x < n; x++)
-		dest[opt++] = src[x];
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
