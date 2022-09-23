80898d9b04f1bd2b9ad2759f648b9fd7bf60d694
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-23 14:04:12 -0700

    function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - string using rot13
+ * @str: string to be encoded
+ * Return: to the string
+ */
+char *rot13(char *str)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(str + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(str + count) == alphabet [i])
+			{
+				*(str + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (str);
+}
