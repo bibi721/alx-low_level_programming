423bef9f62eab46670b086667c169228a934669a
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-26 08:52:40 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,14 @@ unsigned int _strspn(char *s, char *accept)
 	int count = 0;
 
 	int len = 0;
-	int k = 0;
-	
-	while (*accept != '\0')
+
+	while (accept[len] != '\0')
 		len++;
 
-	while (*s != '\0')
-		k++;
 
-
+	
 	for (int i = 0; i < len; i++)
 	{
-		if (count == k)
-			break;
-
 		if (accept[i] == s[count])
 		{
 			count++;
