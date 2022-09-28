dddffc8bff4bd3cf04ac209444b2c3fd0af133eb
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-28 13:41:08 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string s
+ * @s: pointer
+ * Return: recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _length_recursion(++s));
+}
+/**
+ * pali - palidrome func
+ * @s: pointer
+ * @k: place
+ * Return: true or false
+ */
+int pali(char *s, int k)
+{
+	if (k < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + k))
+		return (pali(s + 1, k - 2));
+
+	return (0);
+
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int lgz = _length_recursion(s);
+
+	return (pali(s, lgz - 1));
+}
