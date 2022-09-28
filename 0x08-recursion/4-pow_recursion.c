b69a502d8e2cc0761948708e854ca4550dc994d0
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-28 11:34:46 -0700

    pow(x, y)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,12 +7,14 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int pow = x;
+
 	if (y == 0)
 		return (1);
 
 	else if (y < 0)
 		return (-1);
 
-	return (x * _pow_recursion(x,  y));
+	return (pow * _pow_recursion(x,  y));
 	y--;
 }
