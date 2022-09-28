1cfde49de1c0568e7e6ba40935bce2034c382843
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-09-28 10:05:04 -0700

    string to output func

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: - the string that will br printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
+_putchar('\n');
+}
