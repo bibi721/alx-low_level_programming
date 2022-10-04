9b67ef1688416926e93662856d851764e39f4cb3
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-10-04 06:00:22 -0700

    proprogram that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints its name
+ * @argc: counter
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Alx C language arguments
