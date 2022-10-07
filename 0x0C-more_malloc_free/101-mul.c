b5dfbec61d8a57e213da978f74d47e1105204c47
ermiaskebede721@gmail.com <ermiaskebede721@gmail.com>
2022-10-07 14:21:01 -0700

    argument

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: array argument
+ * @argv: numbers of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long q;
+	int in, z;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (in = 1; in < argc; in++)
+	{
+		for (z = 0; argv[in][z] != '\0'; z++)
+		{
+			if (argv[in][z] > 57 || argv[in][z] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+
+		}
+
+	}
+
+	q = atol(argv[1]) * atol(argv[2]);
+
+	printf("%lu\n", q);
+
+	return (0);
+}
